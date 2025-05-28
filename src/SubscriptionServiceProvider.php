036ba05bf1e208cd3f7d8f5cb8f96e6caba68b8a
Rahim12345 <rahim.suleymanov94@gmail.com>
2025-05-28 12:22:20 +0400

    wip

--- src/SubscriptionServiceProvider.php
+++ src/SubscriptionServiceProvider.php
@@ -8,15 +8,31 @@ class SubscriptionServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Routes
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        // Migration
+        $this->publishes([
+            __DIR__ . '/../database/migrations/create_subscriptions_table.php.stub' => database_path('migrations/' . date('Y_m_d_His') . '_create_subscriptions_table.php'),
+        ], 'migrations');
 
         // Views
-        $this->loadViewsFrom(__DIR__.'/views', 'subscription');
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/subscription-form'),
+        ], 'views');
 
-        // Migrations
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // Controller (istəyə bağlı)
+        $this->publishes([
+            __DIR__ . '/../src/Http/Controllers/SubscriptionController.php' => app_path('Http/Controllers/SubscriptionController.php'),
+        ], 'controllers');
+
+        // Load routes
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+
+        // Load views namespace
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'subscription');
+
+        // Load migrations from package (alternativ olaraq)
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
+
 
     public function register()
     {
